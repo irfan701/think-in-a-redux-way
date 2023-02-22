22ce192a30838ee8dc2a30501e1df8cc5a2d8e17
irfan701 <irfanhossain701@gmail.com>
2023-02-22 15:59:29 +0600

    Array.reduce
    Array.reduce

--- counter_app/src/App.js
+++ counter_app/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Counter from "./components/Counter";
 import Stats from "./components/Stats";
 
+const initialState = [
+    {id: 1, count: 0},
+    {id: 2, count: 0},
+];
+
+
 const App = () => {
+
+    const [state, setState] = useState(initialState)
+    const totalCount = () => {
+        return state.reduce((accumulator, currentValue) => accumulator + currentValue.count,0)
+    }
+
     return (
         <>
             <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
@@ -13,7 +25,7 @@ const App = () => {
                 <div className="max-w-md mx-auto mt-10 space-y-5">
                     <Counter/>
                     <Counter/>
-                    <Stats count={3}/>
+                    <Stats count={totalCount()}/>
                 </div>
             </div>
         </>
